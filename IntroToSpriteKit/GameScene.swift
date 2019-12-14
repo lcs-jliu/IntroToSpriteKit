5e619d9ef647db886f77dcc93d46d352a1cc670f
Jason Liu <jliu@lcs.on.ca>
2019-12-14 17:09:54 -0500

    Added letters "Christmas" and make it drop after the animation

--- IntroToSpriteKit/GameScene.swift
+++ IntroToSpriteKit/GameScene.swift
@@ -15,13 +15,13 @@ class GameScene: SKScene {
     // Background music player
     var backgroundMusic: AVAudioPlayer?
     
-    // This function runs once to set up the scene
+    /// This function runs once to set up the scene
     override func didMove(to view: SKView) {
         
-        // Setting an edge
+        /// Setting an edge
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
         
-        // Set the background
+        /// Set the background
         
         // Default background
         self.backgroundColor = .black
@@ -31,7 +31,7 @@ class GameScene: SKScene {
         background.position = CGPoint(x: background.size.width / 2, y: background.size.height / 2)
         self.addChild(background)
         
-        // Add ground tiles into the animation
+        /// Add ground tiles into the animation
         
         // Add the starting ground tile
         let startGroundTile = SKSpriteNode(imageNamed: "groundTileStart")
@@ -54,14 +54,14 @@ class GameScene: SKScene {
         let backgroundPhysicsBodyLocation = CGRect(x: -1 * background.size.width / 2, y:  -1 * background.size.height / 2 + startGroundTile.size.height, width: background.size.width, height: 1)
         background.physicsBody = SKPhysicsBody(edgeLoopFrom: backgroundPhysicsBodyLocation)
         
-        // Make the scene snow all the time
+        /// Make the scene snow all the time
         if let snow = SKEmitterNode(fileNamed: "Snow") {
             
             snow.position = CGPoint(x: background.size.width / 2, y: background.size.height)
             self.addChild(snow)
         }
         
-        // Add a igloo to the background
+        /// Add a igloo to the background
         let igloo = SKSpriteNode(imageNamed: "Igloo")
         igloo.position = CGPoint(x: background.size.width - igloo.size.width / 2 - 10, y: startGroundTile.size.height + igloo.size.height / 2)
         self.addChild(igloo)
@@ -70,12 +70,12 @@ class GameScene: SKScene {
         igloo.physicsBody = SKPhysicsBody(texture: igloo.texture!, size: igloo.size)
         igloo.physicsBody?.isDynamic = false
         
-        // Add a crystal to the background
+        /// Add a crystal to the background
         let crystal = SKSpriteNode(imageNamed: "Crystal")
         crystal.position = CGPoint(x: background.size.width / 2.5, y: startGroundTile.size.height + crystal.size.height / 2)
         self.addChild(crystal)
         
-        // Add a animated snowman into the scene
+        /// Add a animated snowman into the scene
         
         // Create the snowman and add it in the scene
         let snowman = SKSpriteNode(imageNamed: "iceman_01")
@@ -109,21 +109,21 @@ class GameScene: SKScene {
         let moveUpRepeat = SKAction.repeat(actionWaitThenJump, count: 2)
         snowman.run(moveUpRepeat)
         
-        // Add a tree to the background
+        /// Add a tree to the background
         let tree = SKSpriteNode(imageNamed: "Tree_1")
         tree.position = CGPoint(x: background.size.width / 7, y: startGroundTile.size.height + tree.size.height / 2)
         self.addChild(tree)
         
-        // Drop candies from the top
+        /// Drop candies from the top
         let actionSpawnCandy = SKAction.run(spawnCandy)
         let actionWait = SKAction.wait(forDuration: 0.3)
         let sequenceSpawnThenWait = SKAction.sequence([actionSpawnCandy, actionWait])
         let actionRepeatlyAddSand = SKAction.repeat(sequenceSpawnThenWait, count: 25)
         self.run(actionRepeatlyAddSand)
         
-        // Add greeting words on the animation
+        /// Add greeting words on the animation
         
-        // Wait until all the candies are dropped
+        // Action that will wait for 8 seconds
         let actionWaitEightSceonds = SKAction.wait(forDuration: 8)
         
         // Call the function to create an action
@@ -131,10 +131,22 @@ class GameScene: SKScene {
         
         // Wait for the candies to drop and then for edge
         let sequenceWaitThenFormEdge = SKAction.sequence([actionWaitEightSceonds, actionWordsHorizontalShelf])
-        
         self.run(sequenceWaitThenFormEdge)
         
+        // Create letters "Christmas" that will fall down from the top
         
+        // Give the letters basic properties
+        let letters = SKLabelNode(fontNamed: "Rockwell")
+        letters.fontSize = 100
+        letters.fontColor = .white
+        letters.text = "Christmas"
+        letters.position = CGPoint(x: 400, y: background.size.height + 10)
+        
+        // Add a physics body to the letters
+        letters.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 500, height: 100), center: CGPoint(x: 0 ,y: 30))
+        
+        // Add the letters into the scene
+        self.addChild(letters)
         
         // Get a reference to the mp3 file in the app bundle
         let backgroundMusicFilePath = Bundle.main.path(forResource: "sleigh-bells-excerpt.mp3", ofType: nil)!
@@ -152,7 +164,7 @@ class GameScene: SKScene {
 
     }
     
-    // This function will add a candy randomly
+    /// This function will add a candy randomly
     func spawnCandy() {
         // Generate a random number based on the image names
         let n = Int.random(in: 1...9)
@@ -172,9 +184,9 @@ class GameScene: SKScene {
         self.addChild(candy)
     }
     
-    // This function creates a edge loop for the wrods.
+    /// This function creates a edge loop for the wrods.
     func horizontalShelf() {
-        // Set the horizontal shelf of the ground tiles
+        // Set the horizontal shelf of the words
         let wordsPhysicsBodyLocation = CGRect(x: 0, y: self.size.height / 2 - 20, width: self.size.width, height: 1)
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: wordsPhysicsBodyLocation)
     }
