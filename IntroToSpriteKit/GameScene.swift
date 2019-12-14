a952fdd49f24b74a1c0700ec9be99d7091dd246c
Jason Liu <jliu@lcs.on.ca>
2019-12-14 13:16:57 -0500

    Set the ground tiles

--- IntroToSpriteKit/GameScene.swift
+++ IntroToSpriteKit/GameScene.swift
@@ -18,12 +18,26 @@ class GameScene: SKScene {
     // This function runs once to set up the scene
     override func didMove(to view: SKView) {
         
-        // Set the background colour
+        // Set the background
         self.backgroundColor = .black
         let background = SKSpriteNode(imageNamed: "BG")
         background.position = CGPoint(x: background.size.width / 2, y: background.size.height / 2)
         self.addChild(background)
         
+        //Add ground tiles into the animation
+        let startGroundTile = SKSpriteNode(imageNamed: "groundTileStart")
+        startGroundTile.position = CGPoint(x: startGroundTile.size.width / 2, y: startGroundTile.size.height / 2)
+        self.addChild(startGroundTile)
+        
+        let endGroundTile = SKSpriteNode(imageNamed: "groundTileEnd")
+        endGroundTile.position = CGPoint(x: background.size.width - endGroundTile.size.width / 2, y: endGroundTile.size.height / 2)
+        self.addChild(endGroundTile)
+        
+        for i in 1...8 {
+            let groundTile = SKSpriteNode(imageNamed: "groundTileMiddle")
+            groundTile.position = CGPoint(x: groundTile.size.width / 2 + CGFloat(i) * groundTile.size.width , y: groundTile.size.height / 2)
+            self.addChild(groundTile)
+        }
         
         // Get a reference to the mp3 file in the app bundle
         let backgroundMusicFilePath = Bundle.main.path(forResource: "sleigh-bells-excerpt.mp3", ofType: nil)!
